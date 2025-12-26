4836a3dbb69e165d2efe51904c63cbbcb15daa49
George Watson <gigolo@hotmail.co.uk>
2025-12-26 13:53:27 +0000

    logical cores for windows/macos

--- src/macos/cpu_count.c
+++ src/macos/cpu_count.c
@@ -15,7 +15,28 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
-// macOS uses POSIX sysconf
+/* macOS CPU count using sysctlbyname */
+
 #ifndef HAL_NO_CPU_COUNT
-#include "../cpu_count_posix.c"
+#include "hal/cpu_count.h"
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
+bool hal_cpu_count_available(void) {
+    return true;
+}
+
+static int macos_cpu_count(const char *type) {
+    int num_cores;
+    size_t len = sizeof(num_cores);
+    return sysctlbyname(type, &num_cores, &len, NULL, 0) == 0 ? num_cores : -1;
+}
+
+int hal_cpu_count_logical(void) {
+    return macos_cpu_count("hw.logicalcpu");
+}
+
+int hal_cpu_count_physical(void) {
+    return macos_cpu_count("hw.physicalcpu");
+}
 #endif // HAL_NO_CPU_COUNT
--- src/windows/cpu_count.c
+++ src/windows/cpu_count.c
@@ -15,13 +15,19 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
-// Windows CPU count using GetSystemInfo
+/* Windows CPU count using GetLogicalProcessorInformation
+   Reference: plyer/plyer/platforms/win/cpu.py */
 
 #ifndef HAL_NO_CPU_COUNT
 #include "hal/cpu_count.h"
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <stdlib.h>
+
+/* LOGICAL_PROCESSOR_RELATIONSHIP enum values */
+#define RelationProcessorCore    0
+#define RelationProcessorPackage 3
 
 bool hal_cpu_count_available(void) {
     return true;
@@ -34,9 +40,36 @@ int hal_cpu_count_logical(void) {
 }
 
 int hal_cpu_count_physical(void) {
-    // Windows doesn't have a simple API for physical cores
-    // GetLogicalProcessorInformation can be used but is complex
-    // Return logical count as fallback
-    return hal_cpu_count_logical();
+    DWORD buffer_size = 0;
+    SYSTEM_LOGICAL_PROCESSOR_INFORMATION *buffer = NULL;
+    int core_count = 0;
+    
+    /* First call to get required buffer size */
+    GetLogicalProcessorInformation(NULL, &buffer_size);
+    if (GetLastError() != ERROR_INSUFFICIENT_BUFFER)
+        return hal_cpu_count_logical(); /* Fallback */
+    
+    buffer = (SYSTEM_LOGICAL_PROCESSOR_INFORMATION*)malloc(buffer_size);
+    if (!buffer)
+        return hal_cpu_count_logical(); /* Fallback */
+    
+    /* Second call to get actual data */
+    if (!GetLogicalProcessorInformation(buffer, &buffer_size)) {
+        free(buffer);
+        return hal_cpu_count_logical(); /* Fallback */
+    }
+    
+    /* Count entries with RelationProcessorCore relationship */
+    DWORD offset = 0;
+    SYSTEM_LOGICAL_PROCESSOR_INFORMATION *ptr = buffer;
+    while (offset + sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION) <= buffer_size) {
+        if (ptr->Relationship == RelationProcessorCore)
+            core_count++;
+        offset += sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION);
+        ptr++;
+    }
+    
+    free(buffer);
+    return core_count > 0 ? core_count : hal_cpu_count_logical();
 }
 #endif // HAL_NO_CPU_COUNT
